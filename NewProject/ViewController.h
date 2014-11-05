d48d0301b70431b145a5f0a47326cfe71686fc71
Sean Tillotson <seantillotson@mobile-192-17-204-19.near.uiuc.edu>
2014-11-04 21:26:50 -0600

    stuff

---
+++ NewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewProject
+//
+//  Created by Sean Tillotson on 11/4/14.
+//  Copyright (c) 2014 Sean Tillotson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
