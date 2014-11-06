41723ccc8c9a91683db121201df1b364255d0be5
bmcmorr2 <bmcmorr2@illinois.edu>
2014-11-05 19:41:48 -0600

    commit

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Sean Tillotson on 11/4/14.
+//  Copyright (c) 2014 Sean Tillotson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
