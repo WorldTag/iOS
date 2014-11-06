fdaad16dd9b4b4c917cbe85d19798b7446f7e420
bmcmorr2 <bmcmorr2@illinois.edu>
2014-11-05 19:41:24 -0600

    commit

---
+++ Github_Trial_WorldTag/Github_Trial_WorldTag/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Github_Trial_WorldTag
+//
+//  Created by Curran Bhatia on 11/4/14.
+//  Copyright (c) 2014 Curran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
