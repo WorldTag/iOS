2e6bd72482e5e22df53bc90c1b15d8f27df847cd
bmcmorr2 <bmcmorr2@illinois.edu>
2014-11-12 00:52:26 -0600

    reworked how the app handles location

---
+++ World/Obj-C Map/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  Obj-C Map
+//
+//  Created by Brendan McMorrow on 11/11/14.
+//  Copyright (c) 2014 Loki. All rights reserved.
+//
+
+#ifndef Obj_C_Map_LocationManager_h
+#define Obj_C_Map_LocationManager_h
+
+
+#endif
---
+++ World/Obj-C Map/LocationManager.m
@@ -0,0 +1,9 @@
+//
+//  LocationManager.m
+//  Obj-C Map
+//
+//  Created by Brendan McMorrow on 11/11/14.
+//  Copyright (c) 2014 Loki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
