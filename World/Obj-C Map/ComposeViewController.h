573d3123d2d140ee2108ce1db3dbb400f67c519d
bmcmorr2 <bmcmorr2@illinois.edu>
2014-11-12 16:39:05 -0600

    yolo

---
+++ World/Obj-C Map/ComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController+ComposeViewController.h
+//  Obj-C Map
+//
+//  Created by Brendan McMorrow on 11/12/14.
+//  Copyright (c) 2014 Loki. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface ViewController (ComposeViewController)
+
+@end
---
+++ World/Obj-C Map/ComposeViewController.m
@@ -0,0 +1,13 @@
+//
+//  ViewController+ComposeViewController.m
+//  Obj-C Map
+//
+//  Created by Brendan McMorrow on 11/12/14.
+//  Copyright (c) 2014 Loki. All rights reserved.
+//
+
+#import "ViewController+ComposeViewController.h"
+
+@implementation ViewController (ComposeViewController)
+
+@end
