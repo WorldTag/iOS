253ce61548502e64e50a6f34d3fa361c80aad717
bmcmorr2 <bmcmorr2@illinois.edu>
2014-11-11 20:15:53 -0600

    yolo

--- World/Obj-C Map/AppDelegate.m
+++ World/Obj-C Map/AppDelegate.m
@@ -7,22 +7,26 @@
 //
 
 #import "AppDelegate.h"
-
 @interface AppDelegate ()
 
 @end
 
-@implementation AppDelegate
-
+@implementation AppDelegate {
+    UIScrollView *scrollView;
+    MKPinAnnotationView *topPin;
+    SMCalloutView *calloutView;
+    MKMapView *bottomMapView;
+    MKPinAnnotationView *bottomPin;
+}
 @synthesize window;
 @synthesize viewController;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [window addSubview:viewController.view];
-    [window makeKeyAndVisible];
-    // Override point for customization after application launch.
+
+
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
