202e4b1d72bacf972f31660bd397ad9ca5ccc4a3
bmcmorr2 <bmcmorr2@illinois.edu>
2014-11-12 20:28:51 -0600

    working on adding annotations

--- World/Obj-C Map/ComposeViewController.m
+++ World/Obj-C Map/ComposeViewController.m
@@ -10,9 +10,14 @@
 
 @implementation ComposeViewController
 
+@synthesize locationManager;
+@synthesize composeField;
+@synthesize mapController;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.locationManager = [AppDelegate getLocationManager];
+    self.mapController = [MapViewController getMapViewController];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -21,7 +26,12 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)Post:(id)sender {
-}
 
+
+- (IBAction)Post:(id)sender {
+    self.mapController = [MapViewController getMapViewController];
+    
+    Bubble *newBubble = [[Bubble alloc] initWithTitle:composeField.text AndCoordinate:[locationManager getLocation]];
+    [mapController newPostwithBubble:newBubble];
+}
 @end
